37404664629af7b0892e3b5a08b8234fa074c62f
Alex Ma <alexmajin@hotmail.com>
2024-02-04 20:01:49 -0800

    small fix

--- cal.php
+++ cal.php
@@ -125,8 +125,7 @@ $zodiacInfo = getZodiacSign();
 // Output the results
 echo '<br/>';
 echo '' . $zodiacInfo['zodiacSignChinese'] . PHP_EOL;
-echo '<br/>';
-echo '' . $zodiacInfo['zodiacSign'] . PHP_EOL;
+echo ' ' . $zodiacInfo['zodiacSign'] . PHP_EOL;
 
 
 ?>
